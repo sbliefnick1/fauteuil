db81fd0eb930bded17837134c673463f866e9eac
Soren Bliefnick <sbliefnick@gmail.com>
2018-09-14 14:44:42 -0700

    [FAUTEUIL #6] reworks the etl_master_draft.py to account for the switch from tables to views; also more robust and comprehensive accounting for stored procedures and their dependencies; removed table sensors since DB access comes all at once rather than staggered; closes #6

--- dags/etl_master_draft.py
+++ dags/etl_master_draft.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 
+import os
+import json
 from datetime import datetime, timedelta
 from urllib.parse import quote_plus
 
@@ -7,7 +9,6 @@ import pandas as pd
 import sqlalchemy as sa
 
 from airflow import DAG
-from airflow.operators.sensors import SqlSensor
 from airflow.operators.mssql_operator import MsSqlOperator
 from airflow.operators.sensors import ExternalTaskSensor
 
@@ -16,7 +17,7 @@ from auxiliary.outils import get_secret
 default_args = {
     'owner': 'airflow',
     'depends_on_past': False,
-    'start_date': datetime(2018, 7, 11),
+    'start_date': datetime(2018, 9, 12),
     'email': ['sbliefnick@coh.org'],
     'email_on_failure': False,
     'email_on_retry': False,
@@ -24,49 +25,40 @@ default_args = {
     'retry_delay': timedelta(minutes=2)
     }
 
-dag = DAG('etl_master_draft', default_args=default_args, schedule_interval='@daily')
+dag = DAG('etl_master_rework', default_args=default_args, schedule_interval='@daily')
 
 initial_sql = '''
-select distinct p.name as proc_name
-  ,             t.name as table_name
+select p.name as proc_name
+     , case
+          when replace(p.name, '_Logic', '') = o.name
+              then ''
+          else o.name
+      end as dependency_name
+     , c.num_dependencies
 from sys.procedures p
-  left join sys.sql_dependencies d on p.object_id = d.object_id
-  left join sys.tables t on t.object_id = d.referenced_major_id
-  right join edw_clarity_load_audit a on a.tablename = t.name
-where p.name like '%_clarity_%'
-      and p.name not like '%_dev'
-      and t.name like 'clarity_%'
-      and t.name not like '%_zc_%'
-
-union
-
-select distinct p.name as proc_name
-  ,             t.name as table_name
-from sys.procedures p
-  left join sys.sql_dependencies d on p.object_id = d.object_id
-  left join sys.tables t on t.object_id = d.referenced_major_id
-where p.name like '%_clarity_%'
-      and p.name not like '%_dev'
-      and (t.name like 'ebi_dim_%' or t.name like 'ebi_fact_%')
-      and replace(p.name, '_Logic', '') <> t.name;
-'''
-poke_sql = '''
-select case
-       when LoadType = 'Daily'
-            and cast(LastUpdateDatm as date) = 
-                cast(getdate() as date)
-         then 1
-       when LoadType = 'Weekly'
-            and cast(LastUpdateDatm as date) = 
-                cast(dateadd(day, -1 * ((datepart(weekday, getdate()) % 7) - 1), getdate()) as date) -- sunday
-         then 1
-       else 0
-       end
-from EDW_CLARITY_LOAD_AUDIT
-where TableName = '{0}';
+       left join sys.sql_expression_dependencies d on p.object_id = d.referencing_id
+       left join sys.objects o on o.object_id = d.referenced_id
+       left join (select p2.object_id as proc_id
+                       , count(case when o2.name + '_Logic' <> p2.name then 1 end) as num_dependencies
+                  from sys.procedures p2
+                         left join sys.sql_expression_dependencies d2 on p2.object_id = d2.referencing_id
+                         left join sys.objects o2 on o2.object_id = d2.referenced_id
+                  where (p2.name like 'EBI[_]Dim[_]%[_]Logic' or p2.name like 'EBI[_]Fact[_]%[_]Logic' or p2.name like 'EBI[_]Bridge[_]%[_]Logic')
+                    and p2.name not like '%[_]dev'
+                    and (o2.name like 'ebi[_]dim[_]%' or o2.name like 'ebi[_]fact[_]%' or o2.name like 'ebi[_]bridge[_]%')
+                  group by p2.object_id) c on c.proc_id = p.object_id
+where (p.name like 'EBI[_]Dim[_]%[_]Logic' or p.name like 'EBI[_]Fact[_]%[_]Logic' or p.name like 'EBI[_]Bridge[_]%[_]Logic')
+  and p.name not like '%[_]dev'
+  and (o.name like 'ebi[_]dim[_]%' or o.name like 'ebi[_]fact[_]%' or o.name like 'ebi[_]bridge[_]%')
+  and exists (select 1 from sys.procedures p3 where p3.name = o.name + '_Logic')
+order by p.name, o.name;
 '''
 
-ebi = get_secret('ebi_db_conn')['db_connections']['fi_dm_ebi']
+if os.sys.platform == 'darwin':
+    with open(f'./secrets/ebi_db_conn.json', 'r') as secret_file:
+        ebi = json.load(secret_file)['db_connections']['fi_dm_ebi']
+elif os.sys.platform == 'linux':
+    ebi = get_secret('ebi_db_conn')['db_connections']['fi_dm_ebi']
 
 conn_id = 'ebi_datamart'
 params = quote_plus(f'DRIVER={ebi["driver"]};'
@@ -82,62 +74,42 @@ params = quote_plus(f'DRIVER={ebi["driver"]};'
 engine = sa.create_engine(f'mssql+pyodbc:///?odbc_connect={params}')
 conn = engine.connect()
 
-# get procedure-table relation and unique dfs thereof
+# get procedure-view relationship
 df = pd.read_sql(initial_sql, conn)
-df.table_name = df.table_name.str.lower()
 df.proc_name = df.proc_name.str.lower()
-unique_tables = df.table_name.unique()
+df.dependency_name = df.dependency_name.str.lower()
 unique_procs = df.proc_name.unique()
 
-# remove rows from df where there is no corresponding procedure for an ebi table, e.g., ebi_dim_date
-non_proc_tables = []
-for t in unique_tables:
-    if t.startswith('ebi_') and t + '_logic' not in unique_procs:
-        non_proc_tables.append(t)
+# get tables that have no dependency
+no_dep_procs = df[(df.dependency_name == '') & (df.num_dependencies == 0)][['proc_name']]
 
-for t in non_proc_tables:
-    df = df[df.table_name != t]
+# get tables that have at least one dependency
+dep_procs = df[(df.dependency_name != '') & (df.num_dependencies != 0)][['proc_name', 'dependency_name']]
 
-# create grouping of procedures with the tables they rely on
-proc_map = df.groupby('proc_name')['table_name'].apply(list)
+# create grouping of procs with tables they rely on
+proc_map = dep_procs.groupby('proc_name')['dependency_name'].apply(list)
 
-# create sensors for each of the base tables that starts with 'clarity_'
-sensors = {}
-for t in unique_tables:
-    if t.startswith('ebi_'):
-        continue
-    s = SqlSensor(
-            task_id=f'{t}_sensor',
-            conn_id=conn_id,
-            poke_interval=300,
-            sql=poke_sql.format(t),
-            dag=dag
-            )
-    sensors[t] = s
+# create series of procs that have dependencies to loop over and set downstream
+unique_dep_procs = dep_procs.proc_name.unique()
 
 # create a sqloperator for each procedure
 sql_operators = {}
 for p in unique_procs:
-    tables = proc_map[p]
     o = MsSqlOperator(
             sql=f'exec {p};',
-            task_id=f'load_{p}',
+            task_id=f'exec_{p}',
             mssql_conn_id=conn_id,
             dag=dag
             )
     sql_operators[p] = o
 
-# set procedure downstream from table sensor if table begins with clarity_,
-# and downstream from procedure sqloperator if table begins with ebi_
-for p in unique_procs:
+# set procedures downstream from all their dependencies
+for p in unique_dep_procs:
     tables = proc_map[p]
     for t in tables:
-        if t.startswith('clarity_'):
-            sensors[t] >> sql_operators[p]
-        elif t.startswith('ebi_'):
-            sql_operators[t + '_logic'] >> sql_operators[p]
+        sql_operators[t + '_logic'] >> sql_operators[p]
 
-# create sensor to wait for db_access_daemon to signal that we can log in to database today
+# create sensor to wait for db_access_daemon so we know we can log in to db
 access = ExternalTaskSensor(
         external_dag_id='db_access_daemon',
         external_task_id='attempt_to_connect',
@@ -145,7 +117,5 @@ access = ExternalTaskSensor(
         dag=dag
         )
 
-for t in unique_tables:
-    if t.startswith('ebi_'):
-        continue
-    access >> sensors[t]
+for p in no_dep_procs.proc_name:
+    access >> sql_operators[p]
