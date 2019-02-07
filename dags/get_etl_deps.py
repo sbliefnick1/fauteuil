f4686a9fcbf911802a82e01e5de275bac5666639
Soren Bliefnick <sbliefnick@gmail.com>
2019-02-07 09:43:23 -0800

    Turn on email on failure

--- dags/get_etl_deps.py
+++ dags/get_etl_deps.py
@@ -20,8 +20,8 @@ default_args = {
     'owner': 'airflow',
     'depends_on_past': False,
     'start_date': datetime(2018, 10, 2, tzinfo=pendulum.timezone('America/Los_Angeles')),
-    'email': ['sbliefnick@coh.org'],
-    'email_on_failure': False,
+    'email': ['sbliefnick@coh.org', 'jharris@coh.org'],
+    'email_on_failure': True,
     'email_on_retry': False,
     'retries': 1,
     'retry_delay': timedelta(minutes=2)
